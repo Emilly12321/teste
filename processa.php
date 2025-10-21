b6afc1eab848f6f45027d731b66cc6be39b1f490
Emilly12321 <emillysilvaluz2839@gmail.com>
2025-10-21 00:34:18 -0300

    subindo arquivos

--- processa.php
+++ processa.php
@@ -1,44 +1,3 @@
-<?php
-
-
-session_start();
-
-if (!isset($_SESSION['produtos'])) {
-    $_SESSION['produtos'] = [
-        "nomes" => [],
-        "precos" => [],
-        "estoques" => []
-    ];
-}
-
-
-function voltar()
-{
-    echo "<a href='index.html'>Cadastrar mais</a></div>";
-}
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $nome = $_POST["nome"] ?? "";
-    $preco = (float) $_POST["preco"];
-    $estoque = (int) $_POST["estoque"];
-
-    if ($nome === "" || $preco < 0 ||  $estoque < 0) {
-        echo "<div class='container_resposta'>Favor preencher corretamente os campos!!";
-        voltar();
-        exit;
-    } else {
-        $_SESSION['produtos']['nomes'][] = $nome;
-        $_SESSION['produtos']['precos'][] = $preco;
-        $_SESSION['produtos']['estoques'][] = $estoque;
-    }
-}
-
-
-
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -51,24 +10,57 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 </head>
 
 <body>
-    <header>
 
-        <div>
-            <h1>Supermercado Tchê</h1>
-        </div>
+    <section>
 
-    </header>
-    <main>
+        <form method="POST" action="processa.php">
+            <h1>Cadastro de Estoque</h1>
+            <label for="nome">Nome do Produto:</label>
+            <input type="text" name="nome" id="nome" required>
 
-        <section>
+            <label for="preco">Preço (R$):</label>
+            <input type="number" name="preco" id="preco" step="0.01" required>
+
+            <label for="estoque">Quantidade em Estoque:</label>
+            <input type="number" name="estoque" id="estoque" required>
+
+            <button type="submit">Cadastrar</button>
+
+
 
 
             <?php
 
 
+            session_start();
+
+            if (!isset($_SESSION['produtos'])) {
+                $_SESSION['produtos'] = [
+                    "nomes" => [],
+                    "precos" => [],
+                    "estoques" => []
+                ];
+            }
+
+            if ($_SERVER["REQUEST_METHOD"] === "POST") {
+                $nome = $_POST["nome"] ?? "";
+                $preco = (float) $_POST["preco"];
+                $estoque = (int) $_POST["estoque"];
+
+                if ($nome === "" || $preco < 0 ||  $estoque < 0) {
+                    echo "<div class='container_resposta'>Favor preencher corretamente os campos!!";
+                    exit;
+                } else {
+                    $_SESSION['produtos']['nomes'][] = $nome;
+                    $_SESSION['produtos']['precos'][] = $preco;
+                    $_SESSION['produtos']['estoques'][] = $estoque;
+                }
+            }
+
 
             echo "<div class='container_resposta'>";
             if (!empty($_SESSION['produtos'])) {
+
                 echo "<h2>📦 Estoque de Produtos</h2>
                      <table>
                             <tr>
@@ -82,12 +74,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                     echo "<td>" . number_format($_SESSION['produtos']['precos'][$i], 2, ',', '.') . "</td>";
                     echo "<td>" . $_SESSION['produtos']['estoques'][$i] . "</td></tr>";
                 }
-                echo "</table>";
-                echo voltar();
+                echo "</table></div>";
             }
             ?>
+        </form>
 
-        </section>
+    </section>
     </main>
 
 </body>
